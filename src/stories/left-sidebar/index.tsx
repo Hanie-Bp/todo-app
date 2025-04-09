bd91105dcb84ca49c927a90201d19df67c4dd187
Hanie Bp <haniebaratpoor@gmail.com>
2025-04-09 15:04:46 +0330

    feat/create darkmode  and progress bar

--- src/stories/left-sidebar/index.tsx
+++ src/stories/left-sidebar/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/stories/ui/avatar";
-import Darkmode from "../darkmode";
+import { ModeToggle } from "../darkmode/index";
+import { Progress } from "@/stories/ui/progress";
 
 const LeftSidebar = () => {
   return (
-    <section className="border border-black min-h-screen w-72">
+    <section className="border border-black min-h-screen w-72 p-3 bg-muted">
       <section className="flex items-center justify-center border">
         <h2 className="me-2">Hi , User</h2>
         <Avatar>
@@ -12,7 +13,18 @@ const LeftSidebar = () => {
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
       </section>
-      <Darkmode/>
+
+      <ModeToggle />
+
+      <section className="mt-5">
+        <section className="flex justify-between">
+          <p>All tasks</p>
+          <span>1/2</span>
+        </section>
+        <section className="mt-2">
+          <Progress  className='[&>*]:bg-secondary' value={5} max={10}  />
+        </section>
+      </section>
     </section>
   );
 };
