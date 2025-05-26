9c70175a09d0aa6587df66c17981130ee0e7f1d3
Hanie Bp <haniebaratpoor@gmail.com>
2025-05-26 18:14:53 +0330

    feature/create-auth-components

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,25 @@
+// export default function SignInLayout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <html lang="en">
+//       <body >
+//         <main className="min-h-screen flex items-center justify-center bg-[url(/images/bg-todo.jpg)]" >
+//           {children}
+//         </main>
+//       </body>
+//     </html>
+//   );
+// }
+
+import "../globals.css"
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <main className="min-h-screen flex items-center justify-center bg-[url(/images/bg-todo.jpg)] bg-cover">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import FormSignin from "@/components/form-auth/form-signin";
+import React from "react";
+
+const SignIn = () => {
+  return (
+    <section>
+      <FormSignin />
+    </section>
+  );
+};
+
+export default SignIn;
