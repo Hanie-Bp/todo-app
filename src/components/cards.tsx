887f415de9efc831b4c9aa489ccc4cdce50525d7
Hanie Bp <haniebaratpoor@gmail.com>
2025-06-12 14:55:29 +0330

    feat/fix  task-card loading

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -1,11 +1,11 @@
 "use client";
+import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
 import { Directory, Task } from "@/types/types";
 import { sortTasks } from "@/utils/sortTasks";
 import dynamic from "next/dynamic";
-import { useSearchParams } from "next/navigation";
-import React, { FC } from "react";
-import { useSelector } from "react-redux";
 import { Skeleton } from "./ui/skeleton";
 
 type CardsProps = {
@@ -13,22 +13,26 @@ type CardsProps = {
   directories: Directory[];
 };
 
-const Cards: FC<CardsProps> = ({ tasks, directories }) => {
-  const TaskCard = dynamic(() => import("@/stories/task-card"), {
-    ssr: false,
-    loading: () => (
-      <Skeleton className="h-32 w-[250px] rounded-xlbackdrop-blur-md bg-white/30 rounded-xl shadow-lg " />
-    ),
-  });
+
+
+const loadTaskCard = () => import("@/stories/task-card");
+
+
+const TaskCard = dynamic(loadTaskCard, {
+  ssr: false,
+  loading: () => (
+    <Skeleton className="h-32 w-[250px] rounded-xl backdrop-blur-md bg-white/30 shadow-lg" />
+  ),
+});
+
+const Cards = ({ tasks, directories }: CardsProps) => {
   const viewMode = useSelector((state: RootState) => state.view.mode);
-  const listView = `flex flex-col justify-center items-center gap-5`;
-  const gridView = `grid grid-cols-1 gap-5 place-items-center  overflow-hidden  md:grid-cols-2 xl:grid-cols-3 xl:gap-x-0`;
   const searchParams = useSearchParams();
   const sort = searchParams.get("sort");
-  const sortName = sort?.split(" ")[0].toLocaleLowerCase()!;
+  const sortName = sort?.split(" ")[0].toLowerCase()!;
   const sortedTasks = sortTasks(tasks, sortName);
   const searchValue = searchParams.get("search");
-  console.log("searchValue", searchValue);
+
   const filteredTasks =
     searchValue && searchValue.trim() !== ""
       ? sortedTasks.filter((task) =>
@@ -38,11 +42,19 @@ const Cards: FC<CardsProps> = ({ tasks, directories }) => {
 
   const cardTasks = filteredTasks ?? sortedTasks;
 
+  const listView = `flex flex-col justify-center items-center gap-5`;
+  const gridView = `grid grid-cols-1 gap-5 place-items-center  overflow-hidden  md:grid-cols-2 xl:grid-cols-3 xl:gap-x-0`;
+
+
+  useEffect(() => {
+    loadTaskCard();
+  }, []);
+
   return (
     <section
-      className={`container py-6  mx-auto ${
+      className={`container py-6 mx-auto ${
         viewMode === "grid" ? gridView : listView
-      }  `}
+      }`}
     >
       {cardTasks?.map((task) => (
         <TaskCard
