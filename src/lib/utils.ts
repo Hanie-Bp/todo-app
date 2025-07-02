5eed19d78444d9f121c84c077f4f6495abe93745
Hanie Bp <haniebaratpoor@gmail.com>
2025-07-02 13:11:46 +0330

    delete comments

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,28 +1,9 @@
 import { clsx, type ClassValue } from "clsx";
-import { getServerSession } from "next-auth";
 import { twMerge } from "tailwind-merge";
-import { authOptions } from "./auth";
-// import { getAllDirectories } from "./actions/actions";
-import {  deleteAllData, getAllTasks } from "./actions/task.action";
-import { getAllDirectories } from "./actions/directory.action";
-import { Task } from "@/types/types";
+
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-// export const userSession = async () => {
-//   const session = await getServerSession(authOptions);
-//   return session;
-// };
-
-// export async function fetchDirectories() {
-//   const session = await userSession();
-//   return getAllDirectories(session?.user?.id!);
-// }
-
-// export async function fetchTasks() {
-//   const session = await userSession();
-//   return getAllTasks(session?.user?.id!);
-// }
 
